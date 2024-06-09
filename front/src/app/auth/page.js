90912e8eb85c03061e93ac3aa277ddcbb99a26f9
Mougni-Nawel <82378208+Mougni-Nawel@users.noreply.github.com>
2024-06-09 17:21:18 +0200

    Update page.js

--- front/src/app/auth/page.js
+++ front/src/app/auth/page.js
@@ -49,7 +49,7 @@ const Login = () => {
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   value={email}
-                  onChange={(e) => setUsername(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
             </div>
